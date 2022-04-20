c56b72062c119ea152270644e461b9cb5d97b9d9
eyupkureli <eyupkurelii@gmail.com>
2022-04-20 10:29:02 +0100

    tried to create {symbol}

--- src/Currency.js
+++ src/Currency.js
@@ -2,16 +2,19 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 const Currency = () => {
-  const { symbol} = useParams();
   const [currency, setCurrency] = useState({});
+  
+  const { symbol } = useParams();
+
+  //url !
 
   useEffect(() => {
     fetch(
-      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=symbol&per_page=45&page=1&sparkline=false${symbol}`
+      `https://api.coingecko.com/api/v3/coins/${symbol}`
     )
       .then((response) => response.json())
       .then((currency) => setCurrency(currency));
-  }, [symbol]);
+  }, []);
 
   return (
     <div>
